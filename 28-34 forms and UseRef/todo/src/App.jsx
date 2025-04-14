f60def2ebc63e39d8a84a4fe132fa34eb3e0da09
kkoli-9528 <118213144+kkoli-9528@users.noreply.github.com>
2025-04-14 14:54:50 +0530

    Learned use of useReducer

--- 28-34 forms and UseRef/todo/src/App.jsx
+++ 28-34 forms and UseRef/todo/src/App.jsx
@@ -3,39 +3,18 @@ import AddTodo from "./components/AddTodo";
 import TodoItems from "./components/TodoItems";
 import WelcomeMessage from "./components/WelcomeMessage";
 import "./App.css";
-import { useState } from "react";
-import { TodoItemsContext } from "./store/todo-items-store";
+import TodoItemsContextProvider from "./store/todo-items-store";
 
 function App() {
-  const [todoItems, setTodoItems] = useState([]);
-
-  const addNewItems = (itemName, itemDueDate) => {
-    setTodoItems((currValue) => [
-      ...currValue,
-      { name: itemName, dueDate: itemDueDate },
-    ]);
-  };
-
-  const deleteItem = (todoItemName) => {
-    const newTodoItems = todoItems.filter((item) => item.name !== todoItemName);
-    setTodoItems(newTodoItems);
-  };
-
   return (
-    <TodoItemsContext.Provider
-      value={{
-        todoItems,
-        addNewItems,
-        deleteItem
-      }}
-    >
+    <TodoItemsContextProvider>
       <center className="todo-container">
         <AppName />
         <AddTodo />
         <WelcomeMessage />
         <TodoItems />
       </center>
-    </TodoItemsContext.Provider>
+    </TodoItemsContextProvider>
   );
 }
 
--- 28-34 forms and UseRef/todo/src/store/todo-items-store.jsx
+++ 28-34 forms and UseRef/todo/src/store/todo-items-store.jsx
@@ -1,7 +1,94 @@
 import { createContext } from "react";
+import { useReducer } from "react";
 
 export const TodoItemsContext = createContext({
   todoItems: [],
   addNewItems: () => { },
   deleteItem: () => { },
 });
+
+// todoItemsReducer is a reducer function that takes an action and returns a new state
+// It is used to manage the state of todo items in the application
+// The reducer function takes the current state and an action as arguments
+// and returns a new state based on the action type
+const todoItemsReducer = (currentTodoItems, action) => {
+
+  let newTodoItems = currentTodoItems;
+  if (action.type === "NEW_ITEM") {
+    newTodoItems = [
+      ...currentTodoItems,
+      {
+        name: action.payload.itemName,
+        dueDate: action.payload.itemDueDate,
+      },
+    ];
+  } else if (action.type === "DELETE_ITEM") {
+    newTodoItems = currentTodoItems.filter((item) => item.name !== action.payload.itemName);
+  }
+  return newTodoItems;
+}
+
+const TodoItemsContextProvider = ({ children }) => {
+  // UseReducer is used to manage the state of todo items
+  // It takes a reducer function and an initial state as arguments
+  // The reducer function takes the current state and an action as arguments
+  // and returns a new state based on the action type
+  const [todoItems, dispatchTodoItems] = useReducer(todoItemsReducer, []);
+
+  // AddNewItems function is used to add new items to the todo list
+  // It takes the item name and due date as arguments and dispatches an action to the reducer
+  // The action type is "NEW_ITEM" and the payload contains the item name and due date
+  // The reducer function will then update the state with the new item 
+  const addNewItems = (itemName, itemDueDate) => {
+    const newItemAction = {
+      type: "NEW_ITEM",
+      payload: {
+        itemName,
+        itemDueDate
+      },
+    };
+
+    dispatchTodoItems(newItemAction);
+
+    // The following code is commented out because we are using useReducer instead of useState
+    // setTodoItems((currValue) => [
+    //   ...currValue,
+    //   { name: itemName, dueDate: itemDueDate },
+    // ]);
+  };
+
+  // DeleteItem function is used to delete an item from the todo list
+  // It takes the item name as an argument and dispatches an action to the reducer
+  // The action type is "DELETE_ITEM" and the payload contains the item name
+  // The reducer function will then update the state by filtering out the item with the given name
+  // The reducer function will then return the new state without the deleted item
+  // The deleteItem function is used to delete an item from the todo list
+  const deleteItem = (todoItemName) => {
+    const deleteItemAction = {
+      type: "DELETE_ITEM",
+      payload: {
+        itemName: todoItemName,
+      },
+    };
+
+    dispatchTodoItems(deleteItemAction);
+
+    // The following code is commented out because we are using useReducer instead of useState
+    // const newTodoItems = todoItems.filter((item) => item.name !== todoItemName);
+    // setTodoItems(newTodoItems);
+  };
+
+  return (
+    <TodoItemsContext.Provider
+      value={{
+        todoItems,
+        addNewItems,
+        deleteItem,
+      }}
+    >
+      {children}
+    </TodoItemsContext.Provider>
+  );
+};
+
+export default TodoItemsContextProvider;
