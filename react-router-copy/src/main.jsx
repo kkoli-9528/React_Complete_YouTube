0f355ab0d072854e68b52bd45b4b459438512aed
kkoli-9528 <118213144+kkoli-9528@users.noreply.github.com>
2025-04-20 00:41:55 +0530

    Submitting form data using action

--- react-router-copy/src/main.jsx
+++ react-router-copy/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./routes/App.jsx";
-import CreatePost from "./components/CreatePost.jsx";
+import CreatePost, { createPostAction } from "./components/CreatePost.jsx";
 import PostList, { postLoader } from "./components/PostList.jsx";
 
 const router = createBrowserRouter([
@@ -10,7 +10,11 @@ const router = createBrowserRouter([
     path: "/", element: <App />,
     children: [
       { path: "/", element: <PostList />, loader: postLoader },
-      { path: "/create-post", element: <CreatePost /> }
+      {
+        path: "/create-post",
+        element: <CreatePost />,
+        action: createPostAction
+      },
     ]
   },
 ]);
