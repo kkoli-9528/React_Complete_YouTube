6734e7b35cda2bafc2cddf4c302460088275010c
kkoli-9528 <118213144+kkoli-9528@users.noreply.github.com>
2025-04-23 00:16:28 +0530

    Seperated counter and privacy slices from the store for better understanding of seperation of concerns

--- redux/Counter-React-Redux-Toolkit/src/store/index.js
+++ redux/Counter-React-Redux-Toolkit/src/store/index.js
@@ -1,33 +1,6 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit';
-
-const counterSlice = createSlice({
-  name: 'counter',
-  initialState: { counterVal: 0 },
-  reducers: {
-    increment: (state) => {
-      state.counterVal++;
-    },
-    decrement: (state) => {
-      state.counterVal--;
-    },
-    add: (state, action) => {
-      state.counterVal += Number(action.payload);
-    },
-    substract: (state, action) => {
-      state.counterVal -= Number(action.payload);
-    }
-  }
-});
-
-const privacySlice = createSlice({
-  name: 'privacy',
-  initialState: false,
-  reducers: {
-    toggle: (state) => {
-      return state = !state;
-    }
-  }
-});
+import { configureStore } from '@reduxjs/toolkit';
+import counterSlice from './counter';
+import privacySlice from './privacy';
 
 const counterStore = configureStore({
   reducer: {
@@ -36,29 +9,4 @@ const counterStore = configureStore({
   }
 });
 
-export const counterActions = counterSlice.actions;
-export const privacyActions = privacySlice.actions;
-
 export default counterStore;
-
-
-/*const INITIAL_VALUE = {
-  counter: 0,
-  privacy: false
-};*/
-
-/*const couterReducer = (store = INITIAL_VALUE, action) => {
-  let newCounterValue = store;
-  if (action.type === "INCREMENT") {
-    newCounterValue = { ...store, counter: store.counter + 1 };
-  } else if (action.type === "DECREMENT") {
-    newCounterValue = { ...store, counter: store.counter - 1 };
-  } else if (action.type === "ADDITION") {
-    newCounterValue = { ...store, counter: store.counter + Number(action.payload.num) };
-  } else if (action.type === "SUBSTRACT") {
-    newCounterValue = { ...store, counter: store.counter - Number(action.payload.num) };
-  } else if (action.type === "PRIVACY_TOGGLE") {
-    newCounterValue = { ...store, privacy: !store.privacy };
-  }
-  return newCounterValue;
-};*/
